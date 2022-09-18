5cc88a01347aab845ec6b4b7e303ea4e03952c1b
ibrahimzitouni <76236650+ibrahimzitouni@users.noreply.github.com>
2022-09-18 03:46:35 +0200

    Update RemoteDesktoProtocole.sh

--- RemoteDesktoProtocole.sh
+++ RemoteDesktoProtocole.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 printf "salam !!! ki dayr cava    lmohim sbr xwiya hadchi db ykhdm" >&2
-{
 sudo useradd -m ibrahim
 sudo adduser ibrahim sudo
 echo 'ibrahim:1234' | sudo chpasswd
@@ -27,7 +26,6 @@ cd ~
 curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt install nodejs
-} &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
